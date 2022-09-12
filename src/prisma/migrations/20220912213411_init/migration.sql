cb7d66a0879054d12036e0ad68841657137944e7
Alessandra Canuto <ale.canutto@gmail.com>
2022-09-12 17:48:35 -0400

    Update schema db and add seed

---
+++ src/prisma/migrations/20220912213411_init/migration.sql
@@ -0,0 +1,167 @@
+-- CreateEnum
+CREATE TYPE "role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "gender" AS ENUM ('FEMININE', 'MASCULINE', 'OTHERS');
+
+-- CreateEnum
+CREATE TYPE "type" AS ENUM ('ONLINE', 'PRESENTIAL');
+
+-- CreateEnum
+CREATE TYPE "schedule_type" AS ENUM ('INDIVIDUAL', 'IN_GROUP', 'COUPLE');
+
+-- CreateEnum
+CREATE TYPE "status" AS ENUM ('PENDING', 'REALIZED', 'CANCELED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "role" "role" NOT NULL DEFAULT 'ADMIN',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "professionals" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(150) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "crp" VARCHAR(20),
+    "cellphone" VARCHAR(14),
+    "approach" VARCHAR(60),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "professionals_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patients" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(150) NOT NULL,
+    "cpf" VARCHAR(14) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "gender" "gender" NOT NULL,
+    "cellphone" VARCHAR(15) NOT NULL,
+    "birth_date" DATE NOT NULL,
+    "professional_id" INTEGER NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "schedules" (
+    "id" SERIAL NOT NULL,
+    "session_date" TIMESTAMP(3) NOT NULL,
+    "status" "status" NOT NULL DEFAULT 'PENDING',
+    "type" "type" NOT NULL DEFAULT 'ONLINE',
+    "schedule_type" "schedule_type" NOT NULL,
+    "professional_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "schedules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patients_schedule" (
+    "id" SERIAL NOT NULL,
+    "schedule_id" INTEGER NOT NULL,
+    "patient_id" INTEGER NOT NULL,
+
+    CONSTRAINT "patients_schedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "resources" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(150) NOT NULL,
+    "category" VARCHAR(30) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "resources_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" SERIAL NOT NULL,
+    "subject" VARCHAR(150) NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "schedule_id" INTEGER NOT NULL,
+    "resource_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "professionals_email_key" ON "professionals"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "professionals_crp_key" ON "professionals"("crp");
+
+-- CreateIndex
+CREATE INDEX "professionals_email_idx" ON "professionals"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_cpf_key" ON "patients"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_email_key" ON "patients"("email");
+
+-- CreateIndex
+CREATE INDEX "patients_professional_id_idx" ON "patients"("professional_id");
+
+-- CreateIndex
+CREATE INDEX "patients_email_idx" ON "patients"("email");
+
+-- CreateIndex
+CREATE INDEX "schedules_professional_id_idx" ON "schedules"("professional_id");
+
+-- CreateIndex
+CREATE INDEX "patients_schedule_patient_id_idx" ON "patients_schedule"("patient_id");
+
+-- CreateIndex
+CREATE INDEX "patients_schedule_schedule_id_idx" ON "patients_schedule"("schedule_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "resources_title_category_key" ON "resources"("title", "category");
+
+-- CreateIndex
+CREATE INDEX "sessions_schedule_id_idx" ON "sessions"("schedule_id");
+
+-- AddForeignKey
+ALTER TABLE "professionals" ADD CONSTRAINT "professionals_email_fkey" FOREIGN KEY ("email") REFERENCES "users"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients" ADD CONSTRAINT "patients_professional_id_fkey" FOREIGN KEY ("professional_id") REFERENCES "professionals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients" ADD CONSTRAINT "patients_email_fkey" FOREIGN KEY ("email") REFERENCES "users"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "schedules" ADD CONSTRAINT "schedules_professional_id_fkey" FOREIGN KEY ("professional_id") REFERENCES "professionals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients_schedule" ADD CONSTRAINT "patients_schedule_schedule_id_fkey" FOREIGN KEY ("schedule_id") REFERENCES "schedules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients_schedule" ADD CONSTRAINT "patients_schedule_patient_id_fkey" FOREIGN KEY ("patient_id") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_schedule_id_fkey" FOREIGN KEY ("schedule_id") REFERENCES "schedules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_resource_id_fkey" FOREIGN KEY ("resource_id") REFERENCES "resources"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/prisma/seed.ts
@@ -0,0 +1,273 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient({ log: ['query', 'info'] });
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    email: 'alice@prisma.io',
+    password: '12345678',
+    role: 'ADMIN',
+    Professional: {
+      create: {
+        name: 'Alice Costa',
+      },
+    },
+  },
+  {
+    email: 'nilu@prisma.io',
+    password: 'teste5678',
+    role: 'ADMIN',
+    Professional: {
+      create: {
+        name: 'Nilu Fereira',
+      },
+    },
+  },
+  {
+    email: 'mahmoud@prisma.io',
+    password: '123teste',
+    role: 'ADMIN',
+    Professional: {
+      create: {
+        name: 'Mahmoud Augustu',
+      },
+    },
+  },
+];
+
+const professionalData: Prisma.ProfessionalUpdateArgs[] = [
+  {
+    where: {
+      id: 1,
+    },
+    data: {
+      crp: '1233445565',
+      cellphone: '(67)99998-0000',
+      approach: 'Análise bioenergética',
+    },
+  },
+  {
+    where: {
+      id: 2,
+    },
+    data: {
+      crp: '4556512334',
+      cellphone: '(64)99997-0000',
+      approach: 'Behavorismo',
+    },
+  },
+  {
+    where: {
+      id: 3,
+    },
+    data: {
+      crp: '9999445565',
+      cellphone: '(67)99996-0000',
+      approach: 'Terapia Cognitivo Comportamental (TCC)',
+    },
+  },
+];
+
+const patientData: Prisma.PatientCreateInput[] = [
+  {
+    name: 'Juca Bala',
+    cpf: '00000000091',
+    gender: 'MASCULINE',
+    cellphone: '(67)98100-0009',
+    birthDate: new Date('1990-01-14'),
+    User: {
+      create: {
+        email: 'juca@prisma.io',
+        password: '123teste',
+        role: 'USER',
+      },
+    },
+    Professional: {
+      connect: {
+        id: 1,
+      },
+    },
+  },
+  {
+    name: 'Lara Crispin',
+    cpf: '00000000092',
+    gender: 'FEMININE',
+    cellphone: '(11)98200-0001',
+    birthDate: new Date('1992-06-24'),
+    User: {
+      create: {
+        email: 'lara@prisma.io',
+        password: 'lara123',
+        role: 'USER',
+      },
+    },
+    Professional: {
+      connect: {
+        id: 1,
+      },
+    },
+  },
+  {
+    name: 'Julio Ermiro',
+    cpf: '00000000093',
+    gender: 'OTHERS',
+    cellphone: '(21)98200-0002',
+    birthDate: new Date('1989-09-21'),
+    User: {
+      create: {
+        email: 'julio@prisma.io',
+        password: '546123',
+        role: 'USER',
+      },
+    },
+    Professional: {
+      connect: {
+        id: 3,
+      },
+    },
+  },
+];
+
+const scheduleData: Prisma.ScheduleCreateInput[] = [
+  {
+    sessionDate: new Date('2020-09-20 09:00:00'),
+    scheduleType: 'INDIVIDUAL',
+    Professional: {
+      connect: {
+        id: 1,
+      },
+    },
+    PatientsSchedule: {
+      create: {
+        patientId: 1,
+      },
+    },
+  },
+  {
+    sessionDate: new Date('2020-09-25 11:00:00'),
+    scheduleType: 'COUPLE',
+    Professional: {
+      connect: {
+        id: 2,
+      },
+    },
+    PatientsSchedule: {
+      create: [
+        {
+          patientId: 1,
+        },
+        {
+          patientId: 2,
+        },
+      ],
+    },
+  },
+  {
+    sessionDate: new Date('2020-09-20 09:00:00'),
+    scheduleType: 'IN_GROUP',
+    Professional: {
+      connect: {
+        id: 3,
+      },
+    },
+    PatientsSchedule: {
+      create: [
+        {
+          patientId: 1,
+        },
+        {
+          patientId: 2,
+        },
+        {
+          patientId: 3,
+        },
+      ],
+    },
+  },
+];
+
+const resourceData: Prisma.ResourceCreateInput[] = [
+  { title: 'Depressão', category: 'Transe' },
+  { title: 'Ansiedade', category: 'Ferramenta' },
+  { title: 'Luto', category: 'Transe' },
+];
+
+const sessionData: Prisma.SessionCreateInput[] = [
+  {
+    subject: 'Depressão Psicótica',
+    duration: 30,
+    Schedule: {
+      connect: {
+        id: 1,
+      },
+    },
+    Resource: {
+      connect: {
+        id: 1,
+      },
+    },
+  },
+  {
+    subject: 'Luto Familiar',
+    duration: 60,
+    Schedule: {
+      connect: {
+        id: 2,
+      },
+    },
+    Resource: {
+      connect: {
+        id: 3,
+      },
+    },
+  },
+];
+
+async function main() {
+  for (const user of userData) {
+    await prisma.user.create({
+      data: user,
+    });
+  }
+
+  for (const professional of professionalData) {
+    await prisma.professional.updateMany({
+      where: professional.where,
+      data: professional.data,
+    });
+  }
+
+  for (const patient of patientData) {
+    await prisma.patient.create({
+      data: patient,
+    });
+  }
+
+  for (const schedule of scheduleData) {
+    await prisma.schedule.create({
+      data: schedule,
+    });
+  }
+
+  for (const resource of resourceData) {
+    await prisma.resource.create({
+      data: resource,
+    });
+  }
+
+  for (const session of sessionData) {
+    await prisma.session.create({
+      data: session,
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
