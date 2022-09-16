093f641400a2288a7902a8466e150648b8b908d2
Alessandra Canuto <ale.canutto@gmail.com>
2022-09-16 07:40:07 -0400

    Add professionals routes

---
+++ src/routes/professional.route.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { Routes } from '@interfaces/routes.interface';
+import { ProfessionalController } from '@/controllers/professional.controller';
+import { UpdateProfessionalDto } from '@/dtos/professional.dto';
+import authMiddleware from '@/middlewares/auth.middleware';
+import validationMiddleware from '@middlewares/validation.middleware';
+
+class ProfessionalRoute implements Routes {
+  public path = '/professionals';
+  public router = Router();
+  public professionalController = new ProfessionalController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(
+      `${this.path}/:id(\\d+)`,
+      authMiddleware,
+      this.professionalController.getProfessionalById
+    );
+    this.router.put(
+      `${this.path}/:id(\\d+)`,
+      authMiddleware,
+      validationMiddleware(UpdateProfessionalDto, 'body', true),
+      this.professionalController.updateProfessional
+    );
+  }
+}
+
+export { ProfessionalRoute };
