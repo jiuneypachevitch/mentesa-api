dc6f385d440402b08be04f181ad061cc1235cc2b
Alessandra Canuto <ale.canutto@gmail.com>
2022-09-16 09:58:17 -0400

    Add new routes in swagger

--- src/docs/resources/create.ts
+++ src/docs/resources/create.ts
@@ -1,16 +1,118 @@
 import { components } from '@docs/config';
 
-const { createdResponse, badRequestError, unauthorizedError, conflictError, internalError } =
-  components.responses;
+const {
+  createdResponse,
+  badRequestError,
+  unauthorizedError,
+  conflictError,
+  internalError,
+} = components.responses;
 
 export const create = {
+  '/auth/register': {
+    post: {
+      tags: ['Autenticação'],
+      summary: 'Cadastra usuário',
+      description: 'Rota para a criação de um usuário',
+      requestBody: {
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                name: { type: 'string' },
+                email: { type: 'string' },
+                password: { type: 'string' },
+              },
+              required: ['name', 'email', 'password'],
+              example: {
+                name: 'Ranielle Almeida',
+                email: 'rani@gmail.com',
+                password: '12345678',
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        '201': createdResponse,
+        '400': badRequestError,
+        '401': unauthorizedError,
+        '409': conflictError,
+        '500': internalError,
+      },
+    },
+  },
+  '/auth/login': {
+    post: {
+      tags: ['Autenticação'],
+      summary: 'Loga usuário',
+      description: 'Rota para logar um usuário',
+      requestBody: {
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                email: { type: 'string' },
+                password: { type: 'string' },
+              },
+              required: ['email', 'password'],
+              example: {
+                email: 'rani@gmail.com',
+                password: '12345678',
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        '201': createdResponse,
+        '400': badRequestError,
+        '401': unauthorizedError,
+        '409': conflictError,
+        '500': internalError,
+      },
+    },
+  },
+  '/auth/refresh-token': {
+    post: {
+      tags: ['Autenticação'],
+      security: [{ bearerAuth: [] }],
+      summary: 'Atualiza token de acesso',
+      description: 'Rota para recriar token de acesso',
+      requestBody: {
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                refresh_token: { type: 'string' },
+              },
+              required: ['refresh_token'],
+              example: {
+                refresh_token: '5bc568cc-6bcc-4b79-9dbf-bea5528f8171',
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        '201': createdResponse,
+        '400': badRequestError,
+        '401': unauthorizedError,
+        '409': conflictError,
+        '500': internalError,
+      },
+    },
+  },
   '/resources': {
     post: {
       tags: ['Recursos'],
       security: [{ bearerAuth: [] }],
       summary: 'Cria um recurso',
       description: 'Rota para a criação de um recurso',
-      requestBody: {  
+      requestBody: {
         content: {
           'application/json': {
             schema: {
@@ -39,4 +141,3 @@ export const create = {
     },
   },
 };
-
--- src/docs/resources/getOne.ts
+++ src/docs/resources/getOne.ts
@@ -1,8 +1,32 @@
 import { components } from '@docs/config';
 
-const { successResponse, badRequestError, unauthorizedError, internalError } = components.responses;
+const { successResponse, badRequestError, unauthorizedError, internalError } =
+  components.responses;
 
 export const getOne = {
+  '/professionals/{id}': {
+    get: {
+      tags: ['Profissionais'],
+      security: [{ bearerAuth: [] }],
+      summary: 'Recupera dados do profissinal',
+      description: 'Retorna dados de um profissional',
+      parameters: [
+        {
+          in: 'path',
+          name: 'id',
+          required: true,
+          schema: { type: 'integer' },
+          description: 'Id do profissional',
+        },
+      ],
+      responses: {
+        '200': successResponse,
+        '400': badRequestError,
+        '401': unauthorizedError,
+        '500': internalError,
+      },
+    },
+  },
   '/resources/get/{id}': {
     get: {
       tags: ['Recursos'],
@@ -11,12 +35,12 @@ export const getOne = {
       description: 'Visualizar um recurso específico',
       parameters: [
         {
-            in: 'path',
-            name: 'id',
-            required: true,
-            schema: { type: 'integer'},
-            description: 'Id do recurso'
-        }
+          in: 'path',
+          name: 'id',
+          required: true,
+          schema: { type: 'integer' },
+          description: 'Id do recurso',
+        },
       ],
       responses: {
         '200': successResponse,
@@ -27,4 +51,3 @@ export const getOne = {
     },
   },
 };
-
